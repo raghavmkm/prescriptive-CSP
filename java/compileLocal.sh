ef9927ee741ed886df6e8e886d0520c43e16a625
raghavmkm <78324747+raghavmkm@users.noreply.github.com>
2024-03-16 17:07:50 -0400

    trying to run runAll

---
+++ java/compileLocal.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+########################################
+############# CSCI 2951-O ##############
+########################################
+
+# Update this file with instructions on how to compile your code
+javac -classpath /Applications/CPLEX_Studio2211/cpoptimizer/lib/ILOG.CP.jar ./src/solver/cp/*.java
